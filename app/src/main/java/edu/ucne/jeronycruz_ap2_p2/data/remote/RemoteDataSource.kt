79b996eabea0638670ca7b89b20b6ee901371e3a
jerony <jerony_cruz@ucne.edu.do>
2025-06-19 14:43:14 -0400

    Modificacion en RemoteDataSource.

--- app/src/main/java/edu/ucne/jeronycruz_ap2_p2/data/remote/RemoteDataSource.kt
+++ app/src/main/java/edu/ucne/jeronycruz_ap2_p2/data/remote/RemoteDataSource.kt
@@ -1,12 +1,10 @@
 package edu.ucne.jeronycruz_ap2_p2.data.remote
 
-import edu.ucne.jeronycruz_ap2_p2.data.remote.dto.RepositoryDto
+
 import javax.inject.Inject
 
 class RemoteDataSource @Inject constructor(
     private val gitHubApi: GitHubApi
 ) {
-    suspend fun getRepositories(username: String): List<RepositoryDto> {
-        return gitHubApi.listRepos(username)
-        }
+    suspend fun getRepository(username: String) = gitHubApi.listRepos(username)
 }
