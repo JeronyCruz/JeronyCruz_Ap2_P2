d8c0591c52a5058136e6874759181c3b476203a3
jerony <jerony_cruz@ucne.edu.do>
2025-06-19 14:44:55 -0400

    Modificacion en RepositoryUiState.kt.

--- app/src/main/java/edu/ucne/jeronycruz_ap2_p2/presentation/repository/RepositoryUiState.kt
+++ app/src/main/java/edu/ucne/jeronycruz_ap2_p2/presentation/repository/RepositoryUiState.kt
@@ -1,2 +1,12 @@
 package edu.ucne.jeronycruz_ap2_p2.presentation.repository
 
+import edu.ucne.jeronycruz_ap2_p2.data.remote.dto.RepositoryDto
+
+data class RepositoryUiState (
+    val name: String = "",
+    val descripcion: String = "",
+    val html_url: String = "",
+    val isLoading: Boolean = false,
+    val repository: List<RepositoryDto> = emptyList(),
+    val errorMessage: String? = null,
+)
