bef810d40199253a0311243462d580a520ac7655
Jerony Ulises Cruz Fermin <jerony098@gmail.com>
2025-06-24 23:13:00 -0400

    Agregando filtro de busqueda en RepositoryViewModel.

--- app/src/main/java/edu/ucne/jeronycruz_ap2_p2/presentation/repository/RepositoryViewModel.kt
+++ app/src/main/java/edu/ucne/jeronycruz_ap2_p2/presentation/repository/RepositoryViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import edu.ucne.jeronycruz_ap2_p2.data.remote.Resource
+import edu.ucne.jeronycruz_ap2_p2.data.remote.dto.RepositoryDto
 import edu.ucne.jeronycruz_ap2_p2.data.repository.RepositoryRepository
 import edu.ucne.jeronycruz_ap2_p2.presentation.UiEvent
 import kotlinx.coroutines.channels.Channel
@@ -13,6 +14,9 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
@@ -29,11 +33,54 @@ class RepositoryViewModel @Inject constructor(
     private val _loading = MutableStateFlow(false)
     val loading: StateFlow<Boolean> = _loading
 
+    private val _searchQuery = MutableStateFlow("")
+    val searchQuery: StateFlow<String> = _searchQuery.asStateFlow()
+
+    private val _searchResults = MutableStateFlow<List<RepositoryDto>>(emptyList())
+    val searchResults: StateFlow<List<RepositoryDto>> = _searchResults.asStateFlow()
+
     init {
         getRepository("enelramon")
+        //Para la busqueda
+        viewModelScope.launch {
+            _searchQuery
+                .debounce(600)
+                .distinctUntilChanged()
+                .mapLatest { query ->
+                    filterRepositories(query)
+                }
+                .collectLatest { filtered ->
+                    _searchResults.value = filtered
+                }
+            }
+        }
+
+    fun OnEvent(event: RepositoryEvent){
+        when(event){
+            RepositoryEvent.DeleteRepositories -> TODO()
+            RepositoryEvent.GetRepositories -> getRepository("enelramon")
+            RepositoryEvent.PostRepository -> TODO()
+            RepositoryEvent.PutRepositories -> TODO()
+        }
     }
 
-    fun getRepository(username: String) {
+    fun onSearchQueryChanged(query: String) {
+        _searchQuery.value = query
+    }
+
+
+    private fun filterRepositories(query: String): List<RepositoryDto> {
+        return if (query.isBlank()) {
+            _uiState.value.repository
+        } else {
+            _uiState.value.repository.filter {
+                it.name.contains(query, ignoreCase = true) ||
+                        (it.description?.contains(query, ignoreCase = true) ?: false)
+            }
+            }
+        }
+
+    private fun getRepository(username: String) {
         viewModelScope.launch {
             repositoryRepository.getRepositories(username).collectLatest { getting ->
                 when (getting) {
