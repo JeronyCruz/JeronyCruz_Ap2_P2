e957cecbe03f2f4b57c52db9be0350ddb2e64dfa
JeronyCruz <jerony_cruz@ucne.edu.do>
2025-07-09 21:14:32 -0400

    Se agrego el endpoint para listar los contribuidores.

--- app/src/main/java/edu/ucne/jeronycruz_ap2_p2/data/remote/GitHubApi.kt
+++ app/src/main/java/edu/ucne/jeronycruz_ap2_p2/data/remote/GitHubApi.kt
@@ -1,5 +1,6 @@
 package edu.ucne.jeronycruz_ap2_p2.data.remote
 
+import edu.ucne.jeronycruz_ap2_p2.data.remote.dto.ContributorDto
 import edu.ucne.jeronycruz_ap2_p2.data.remote.dto.RepositoryDto
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -7,4 +8,10 @@ import retrofit2.http.Path
 interface GitHubApi {
     @GET("users/{username}/repos")
     suspend fun listRepos(@Path("username") username: String): List<RepositoryDto>
+
+    @GET("repos/{owner}/{repo}/contributors")
+    suspend fun listContributors(
+        @Path("owner") owner: String,
+        @Path("repo") repo: String
+    ): List<ContributorDto>
 }
