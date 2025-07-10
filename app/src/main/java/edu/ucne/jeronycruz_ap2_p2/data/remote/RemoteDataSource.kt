70cf2f539436e044094f934b57fcce1aabfa0801
JeronyCruz <jerony_cruz@ucne.edu.do>
2025-07-09 21:15:23 -0400

    Se agrego la funcion para listar los contribuidores.

--- app/src/main/java/edu/ucne/jeronycruz_ap2_p2/data/remote/RemoteDataSource.kt
+++ app/src/main/java/edu/ucne/jeronycruz_ap2_p2/data/remote/RemoteDataSource.kt
@@ -7,4 +7,7 @@ class RemoteDataSource @Inject constructor(
     private val gitHubApi: GitHubApi
 ) {
     suspend fun getRepository(username: String) = gitHubApi.listRepos(username)
+
+    suspend fun getContributors(owner: String, repo: String) =
+        gitHubApi.listContributors(owner, repo)
 }
